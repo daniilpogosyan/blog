74b89c826e97dca584a7ae39a66b1b140d0b686d
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-10-17 22:17:44 +0500

    Create functions for storing JWT

---
+++ src/storage/jwt.js
@@ -0,0 +1,32 @@
+import { getStorage } from "./utils";
+
+const storage = getStorage();
+
+export function setJWT(token) {
+  try {
+    storage.setItem('auth-token', token);
+  } catch (err) {
+    return false
+  }
+
+  return true;
+}
+
+export function getJWT() {
+  try {
+    return storage.getItem('auth-token');
+  } catch (err) {
+    return false
+  }
+}
+
+export function removeJWT() {
+  try {
+    storage.removeItem('auth-token');
+  } catch (err) {
+    return false
+  }
+  
+  return true;
+}
+
