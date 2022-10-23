ea6fdea9490ea1bfa828a11885d630b1d6dab00e
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-10-23 15:05:06 +0500

    Create dropdown in userBar with log-in form

--- src/pages/Root/UserBar.jsx
+++ src/pages/Root/UserBar.jsx
@@ -1,15 +1,21 @@
-import { useContext, useEffect } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useFetcher, Link  } from 'react-router-dom';
 
 import CurrentUserContext from '../../contexts/CurrentUserContext';
 import { getJWT } from '../../storage/jwt';
 
 import LogoutButton from './LogoutButton';
+import style from './UserBar.module.css';
 
 export default function UserBar() {
   const [currentUser, setCurrentUser] = useContext(CurrentUserContext);
+  const [isToggled, setIsToggled] = useState(false);
+
   const fetcher = useFetcher();
   
+  function toggleDropdown() {
+    setIsToggled(isToggled ? false : true);
+  }
 
   useEffect(() => {
     // fetcher.state is 'loading' when fetcher action is done
@@ -43,24 +49,33 @@ export default function UserBar() {
 
   return (
     <div>
-      <fetcher.Form action="/login" method="post">
-        <label htmlFor="login-email">Email:</label>
-        <input
-          id="login-email"
-          type="email"
-          placeholder="youremail@somemail.com"
-          name="email"
-        />
-        <label htmlFor="login-password">Password:</label>
-        <input
-          id="login-password"
-          type="password"
-          placeholder="Password"
-          name="password"
-        />
-        <button>Log in</button>
-      </fetcher.Form>
-      <Link to='/signup'>Sign up</Link>
+      <button
+        onClick={toggleDropdown}
+      >
+        Log in
+      </button>
+      <div className={style["dropdown-container"]}>
+        <div className={`${style.dropdown} ${isToggled ? style.toggled: ''}`}>
+          <fetcher.Form action="/login" method="post">
+            <label htmlFor="login-email">Email:</label>
+            <input
+              id="login-email"
+              type="email"
+              placeholder="youremail@somemail.com"
+              name="email"
+            />
+            <label htmlFor="login-password">Password:</label>
+            <input
+              id="login-password"
+              type="password"
+              placeholder="Password"
+              name="password"
+            />
+            <button>Log in</button>
+          </fetcher.Form>
+          <Link to='/signup'>Sign up</Link>
+        </div>
+      </div>
     </div>
   );
 }
