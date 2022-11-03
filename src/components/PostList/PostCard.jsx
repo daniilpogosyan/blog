1007e626952769b3f5afa85fa1469963e9110177
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-11-03 22:23:20 +0500

    Add a status tag to postCard

--- src/components/PostList/PostCard.jsx
+++ src/components/PostList/PostCard.jsx
@@ -2,12 +2,16 @@ import { Link } from 'react-router-dom';
 
 import style from './PostCard.module.css';
 
-export default function PostCard({ title, author, url }) {
+export default function PostCard({ title, author, url, status }) {
+  const statusTagClassName = `${style['status-tag']} ${style[status]}`;
   return (
     <div className={style.card}>
       <h2 className={style.heading}>{title}</h2>
       <p className={style.author}>by {author.username}</p>
       <Link to={url} className={style.link}>Read post</Link>
+      <div className={statusTagClassName}>
+        {status}
+      </div>
     </div>
   )
 }
