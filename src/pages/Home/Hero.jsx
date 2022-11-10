1d414a106ac6259bda36696aa9235e8281615905
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-11-10 22:48:44 +0500

    Add image and text to Hero

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -5,12 +5,14 @@ export default function Hero() {
     <div className={style['container']}>
       <div className={`${style['side']} ${style['drop-shadow-right']}`}>
         <div className={`${style['blank']} ${style['blank-right']}`}></div>
-        <div className={`${style['left']} ${style['light']}`}>Lorem ipsum dolor sit, amet, iste est sit!</div>
+        <div className={`${style['left']} ${style['light']}`}>
+          Share your thoughts with the world!
+        </div>
       </div>
   
       <div className={style['side']}>
         <div className={`${style['blank']} ${style['blank-left']}`}></div>
-        <div className={style['right']}>1 2 3 4 5 6 7 8 9 10</div>
+        <div className={style['right']}></div>
       </div>
     </div>
   )
