91bddf5b173ee48accb506076f28458ec52071a2
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-11-14 21:51:54 +0500

    Render post body as HTML rather than plain text
    Now that post bodies are stored as HTML, It's required to set post data
    as innerHtml, which is in react called dangerouslySetInnerHTML.
    Not sure about security due to possibility of XSS attacks

--- src/pages/PostWithComments/Post.jsx
+++ src/pages/PostWithComments/Post.jsx
@@ -4,7 +4,12 @@ export default function Post({ title, body }) {
   return (
     <div>
       <h1 className={style.heading}>{title}</h1>
-      <p className={style.body}>{body}</p>
+      <div
+        className={style.body}
+        // It's required to use `dangerouslySetInnerHTML` since
+        // bodies of the posts are stored as HTML in order to keep markup
+        dangerouslySetInnerHTML={{__html: body}}
+      />
     </div>
   )
 }
