68888cb868e05bfa4b39e6dccc340d82083ba4d4
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-11-14 21:43:43 +0500

    Add TinyMCE to the form in NewPost route

--- src/pages/NewPost.jsx
+++ src/pages/NewPost.jsx
@@ -1,4 +1,5 @@
 import { Form, redirect, useActionData } from 'react-router-dom';
+import { Editor } from '@tinymce/tinymce-react';
 import { getJWT } from '../storage/jwt';
 
 export default function NewPost() {
@@ -13,8 +14,23 @@ export default function NewPost() {
           <p>{errors?.title?.message}</p>
         </div>
         <div>
-          <label htmlFor="new-post-body">Body:</label>
-          <textarea name="body" id="new-post-body" cols="30" rows="10"></textarea>
+          <Editor
+            textareaName='body'
+            apiKey='gy3e8g7jg729arbox9mtac6bd3zi1quvxdartjwolvpmednb'
+            init={{
+              height: 500,
+              plugins: [
+                'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+                'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+                'insertdatetime', 'media', 'table', 'code',  'wordcount'
+              ],
+              toolbar: 'undo redo | blocks | ' +
+                'bold italic forecolor | alignleft aligncenter ' +
+                'alignright alignjustify | bullist numlist outdent indent | ' +
+                'removeformat | help',
+              content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+            }}
+          />
           <p>{errors?.body?.message}</p>
         </div>
         <button>Create</button>
