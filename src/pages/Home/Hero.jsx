83460c43d067d7e649a84918e8c0077cc421eacb
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-11-30 23:31:37 +0500

    Replace blank divs with pseudo-elements
    This technique allow to get rid of extra markup. Also separated clipper
    from content. It will help to use media query easier.

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -3,16 +3,18 @@ import style from './Hero.module.css';
 export default function Hero() {
   return (
     <div className={style['container']}>
-      <div className={`${style['side']} ${style['drop-shadow-right']}`}>
-        <div className={`${style['blank']} ${style['blank-right']}`}></div>
-        <div className={`${style['left']} ${style['light']}`}>
-          Share your thoughts with the world!
+      <div className={`${style['side']} ${style['left']} ${style['drop-shadow-right']}`}>
+        <div className={`${style['clipper']} ${style['light']}`}>
+          <div className={style['content']}>
+            Share your thoughts with the world!
+          </div>
         </div>
       </div>
   
-      <div className={style['side']}>
-        <div className={`${style['blank']} ${style['blank-left']}`}></div>
-        <div className={style['right']}></div>
+      <div className={`${style['side']} ${style['right']}`}>
+        <div className={style['clipper']}>
+        <div className={style['content']}></div>
+        </div>
       </div>
     </div>
   )
