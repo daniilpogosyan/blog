0db52b5ee4c04e7cb0818113f48031f3c75e5b5c
Daniil Pogosyan <daniilpogosyan@gmail.com>
2022-12-08 23:42:12 +0500

    Implement getPost()

---
+++ src/apis/blog/getPost.js
@@ -0,0 +1,26 @@
+import httpClient from '../../http-client';
+import { getBearerToken } from '../../storage/jwt';
+import { BASEURL } from './utils';
+
+export default async function getPost(postId, params = {}) {
+  if (typeof postId !== 'string' || postId.length === 0) {
+    throw new Error('`postId` must be a non-empty string`');
+  }
+
+  const url = new URL(`posts/${postId}`, BASEURL);
+  const paramsEntries = Object.entries(params);
+  Object.entries(params).forEach(([param, value]) => {
+    url.searchParams.set(param, value);
+  });
+
+  
+  const options = {};
+  if (params.author === 'me') {  
+    options.headers = {
+      Authorization: getBearerToken()
+    }
+  }
+  const response = await httpClient.get(url, options);
+  
+  return response.json();
+}
